4b0065146f1ffbadc3741bb22d779358f0766c55
amircoll1235 <amir_abukar@outlook.com>
2024-12-09 14:42:00 +0100

    emoji dyker upp på fyrkanten

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,15 @@ document.querySelector("button")?.addEventListener("click", () =>
     confettiNumber: 500,
   })
 );
+
+const emoji = ['🐶', '🐱', '🦊', '🐼', '🦁'];
+let currentEmojiIndex = 0; 
+
+document.getElementById("press")?.addEventListener("click", () => {
+    const fyrkantDiv = document.getElementById('fyrkant');
+    if (fyrkantDiv) {
+        currentEmojiIndex = (currentEmojiIndex + 1) % emoji.length; 
+        fyrkantDiv.textContent = emoji[currentEmojiIndex]; 
+    }
+});
+
