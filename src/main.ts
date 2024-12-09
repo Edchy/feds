c7d7428ce4e4b28ea96dfd24a92305754b77ce06
monsterberget <manspbk@gmail.com>
2024-12-09 14:38:41 +0100

    småsaker i html

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,8 @@
 import JSConfetti from "js-confetti";
 
-const html = document.documentElement;
-const lightTheme = document.querySelector(".theme-light");
-const darkTheme = document.querySelector(".theme-dark");
-
-lightTheme?.addEventListener("click", () => {
-  html.style.setProperty("color-scheme", "light");
-});
-darkTheme?.addEventListener("click", () => {
-  html.style.setProperty("color-scheme", "dark");
-});
-
 const jsConfetti = new JSConfetti();
 
-document.querySelector("skull-btn")?.addEventListener("click", () =>
+document.querySelector("button")?.addEventListener("click", () =>
   jsConfetti.addConfetti({
     emojis: ["☠️"],
     confettiRadius: 6,
