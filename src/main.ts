b75fe15730bf9bbf375f26d8bd3b805d92c4e0c6
edch <soderblomed@gmail.com>
2024-12-11 09:51:16 +0100

    add todos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import JSConfetti from "js-confetti";
+import { todos } from "./todo";
 
 const html = document.documentElement;
 const lightTheme = document.querySelector(".theme-light");
 const darkTheme = document.querySelector(".theme-dark");
+const todosList = document.querySelector(".todos-list");
+const todosForm = document.querySelector(".todos-form") as HTMLFormElement;
+
+todosForm?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let newLi = document.createElement("li");
+  const input = todosForm.querySelector("input");
+  if (input) {
+    newLi.textContent = input.value;
+  }
+  todosList?.append(newLi);
+  todosForm.reset();
+});
+
+todos.forEach((todo) => {
+  let li = document.createElement("li");
+  li.textContent = todo;
+  todosList?.append(li);
+});
 
 lightTheme?.addEventListener("click", () => {
   html.style.setProperty("color-scheme", "light");
@@ -21,14 +41,13 @@ document.querySelector(".skull-btn")?.addEventListener("click", () =>
   })
 );
 
-const emoji = ['🐶', '🐱', '🦊', '🐼', '🦁'];
-let currentEmojiIndex = 0; 
+const emoji = ["🐶", "🐱", "🦊", "🐼", "🦁"];
+let currentEmojiIndex = 0;
 
 document.getElementById("press")?.addEventListener("click", () => {
-    const fyrkantDiv = document.getElementById('fyrkant');
-    if (fyrkantDiv) {
-        currentEmojiIndex = (currentEmojiIndex + 1) % emoji.length; 
-        fyrkantDiv.textContent = emoji[currentEmojiIndex]; 
-    }
+  const fyrkantDiv = document.getElementById("fyrkant");
+  if (fyrkantDiv) {
+    currentEmojiIndex = (currentEmojiIndex + 1) % emoji.length;
+    fyrkantDiv.textContent = emoji[currentEmojiIndex];
+  }
 });
-
